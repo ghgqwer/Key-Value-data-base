ad6e79d79a37d574769ddfd0110e4543c3e7f1f8
Vadim Shubin <vbmshbn@yandex.ru>
2024-10-13 23:23:04 +0300

    LPOP done

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	s, err := storage.NewStorage() //s
+	s, err := storage.NewStorage()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -21,25 +21,33 @@ func main() {
 
 	s.Lpush("first", []string{"1", "2", "3"})
 	llst := s.Lpush("first", []string{"4", "5"})
-
 	s.Rpush("second", []string{"1"})
 	rlst := s.Rpush("second", []string{"2", "3"})
+	fmt.Println(llst, rlst)
+	s.Raddtoset("second", []string{"3", "5", "8", "4", "8", "6"})
 
-	s.Raddtoset("second", []string{"3", "5", "8", "4", "8"})
-
-	chahge_lst := s.Check_arr("second")
-
-	//1. Создать словарь с ключами - исходными значениями нашего настоящего словваря
-	//2. Идем циклом по новым значениями и если такого ключа не существует то добавляем к исходному списку новое значение, которое заведомо уникально
+	s.Rpush("third", []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
+	fmt.Println(s.Check_arr("third"))
+	fisrt_step := s.Lpop("third", 2)
+	fmt.Println(fisrt_step)
+	second_step := s.Lpop("third", 2, -2)
+	fmt.Println(second_step)
+	fmt.Println(s.Check_arr("third"))
 
 	res_getkindstr := s.GetKind("string_val")
 	res_getkindint := s.GetKind("int_val")
 	res_getkind_unkonown := s.GetKind("unknown")
 	fmt.Println(res_str, res_int, res_unknown_val)
 	fmt.Println(res_getkindstr, res_getkindint, res_getkind_unkonown)
-	fmt.Println(llst, rlst)
-	fmt.Println(chahge_lst)
 }
+
+//list = [1, 2, 3, 4]
+//del [0, s]
+//s = 1
+//s = 4
+
+//1. Создать словарь с ключами - исходными значениями нашего настоящего словваря
+//2. Идем циклом по новым значениями и если такого ключа не существует то добавляем к исходному списку новое значение, которое заведомо уникально
 
 // func check() {
 // 	true_lst := []string{
