708abcf15325e88c42c6c0a867cfdb5dc6919046
ghgqwer <93539026+ghgqwer@users.noreply.github.com>
2024-10-15 17:35:34 +0300

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
-	"project_1/internal/storage/storage" //!
+	"project_1/internal/storage/storage" 
 )
 
 func main() {
-	//atamarniy operation
 	s, err := storage.NewStorage()
 	if err != nil {
 		log.Fatal(err)
@@ -78,57 +77,3 @@ func main() {
 	}
 
 }
-
-//list = [1, 2, 3, 4]
-//del [0, s]
-//s = 1
-//s = 4
-
-//1. Создать словарь с ключами - исходными значениями нашего настоящего словваря
-//2. Идем циклом по новым значениями и если такого ключа не существует то добавляем к исходному списку новое значение, которое заведомо уникально
-
-// func check() {
-// 	true_lst := []string{
-// 		"first", "second",
-// 	}
-// 	// fmt.Println(true_lst)
-// 	lst := []string{
-// 		"first", "fourth",
-// 	}
-
-// 	fmt.Println(true_lst)
-
-// 	set := make(map[string]struct{})
-
-// 	for _, value := range true_lst {
-// 		set[value] = struct{}{}
-// 	}
-
-// 	for _, val := range lst {
-// 		if _, err := set[val]; !err {
-// 			true_lst = append(true_lst, val)
-// 		}
-// 	}
-// 	fmt.Println(true_lst)
-// }
-
-// true_lst := map[string][]string{
-// 	"first": {"1", "2"},
-// }
-// // fmt.Println(true_lst)
-
-// key := "first"
-// list := []string{"1", "113"}
-
-// new_set := make(map[string]struct{})
-// for _, value := range true_lst[key] {
-// 	new_set[value] = struct{}{}
-// }
-
-// for _, val := range list {
-// 	if _, err := new_set[val]; !err {
-// 		true_lst[key] = append(true_lst[key], val)
-// 	}
-// }
-
-//fmt.Println(true_lst)
