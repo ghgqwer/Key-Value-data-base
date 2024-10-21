964639458a74228044811dcbce41b6a39eff6969
Vadim Shubin <vbmshbn@yandex.ru>
2024-10-21 12:22:16 +0300

    http requests add

--- cmd/main.go
+++ cmd/main.go
@@ -11,11 +11,6 @@ func main() {
 		panic(err)
 	}
 
-	// store.Rpush("first", []string{"1", "2", "3"})
-	// fmt.Println(store.Check_arr("first"))
-	// store.Lpop("first", []int{1})
-	// fmt.Println(store.Check_arr("first"))
-
 	store.ReadFromJSON("data.json")
 	serv := server.New(":8090", &store)
 	serv.Start()
